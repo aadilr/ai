f88d0c324b944a349be48b1a9f2018c116a2ebe3
aadilr <aadil.razvi@gmail.com>
2024-04-21 14:36:12 -0500

    Update setup_miner.sh
    added automated inputs for geographic area and timezone

--- setup_miner.sh
+++ setup_miner.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Automated selection for geographic area (2 for Americas) and timezone (87)
+echo "2" > /tmp/geo_area.txt
+echo "87" > /tmp/time_zone_num.txt
+
 # Update the system
 apt-get update
 apt-get upgrade -y
@@ -17,6 +21,7 @@ pip install -r requirements.txt
 pip install python-dotenv toml diffusers schedule transformers boto3
 
 # Install Python 3.8 and set up virtual environment (if necessary)
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install -y python3.8-venv
 python3.8 -m venv venv
 source venv/bin/activate
@@ -25,8 +30,12 @@ source venv/bin/activate
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
 
 # Set up the environment variables
-echo “MINER_ID_0=0xe3B4Edd1Be17cC655b6973277C96321c907AbeE4” > .env
+echo "MINER_ID_0=0xe3B4Edd1Be17cC655b6973277C96321c907AbeE4" > .env
 
-# Run the miner starter script
+# Run the miner starter script with automated inputs for geographic area and timezone
 chmod +x llm-miner-starter.sh
-./llm-miner-starter.sh openhermes-mixtral-8x7b-gptq
+./llm-miner-starter.sh openhermes-mixtral-8x7b-gptq < /tmp/geo_area.txt < /tmp/time_zone_num.txt
+
+# Clean up
+rm /tmp/geo_area.txt
+rm /tmp/time_zone_num.txt
